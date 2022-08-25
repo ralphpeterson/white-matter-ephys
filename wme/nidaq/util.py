4a3c4bb78d3847063b6eab03b7b25d7c21124935
ralphpeterson <ralph.emilio.peterson@gmail.com>
2022-08-24 23:38:45 -0400

    misc updates

---
+++ wme/nidaq/util.py
@@ -0,0 +1,17 @@
+import h5py
+
+def get_wm_trigger(nidaq_file):
+    """
+    Search the nidaq .h5 file for the trigger signal from White Matter.
+
+    Parameters:
+        nidaq_file: str, pth
+    Reutrns:
+        ephys_trigger: int
+            wm_trigger signal (nidaq clock)
+    """
+
+    data = h5py.File(nidaq_file, 'r')
+    wm_trigger = data['ephys_trigger'][0]
+
+    return wm_trigger
